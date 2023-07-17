2cfc554cf712c3a8aadc61d418ad579582144bdb
Darío Blanco <dblancoit@gmail.com>
2023-07-17 19:22:26 +0200

    chore: add make validate to pre commit hook if yaml files change

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -40,27 +40,27 @@ mkdir -p /tmp/flux-crd-schemas/master-standalone-strict
 curl -sL https://github.com/fluxcd/flux2/releases/latest/download/crd-schemas.tar.gz | tar zxf - -C /tmp/flux-crd-schemas/master-standalone-strict
 
 find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
-  do
-    echo "INFO - Validating $file"
-    yq e 'true' "$file" > /dev/null
+do
+	echo "INFO - Validating $file"
+	yq e 'true' "$file" > /dev/null
 done
 
 echo "INFO - Validating clusters"
 find ./clusters -maxdepth 2 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
-  do
-    kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}" "${file}"
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
-    fi
+do
+	kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}" "${file}"
+	if [[ ${PIPESTATUS[0]} != 0 ]]; then
+	  exit 1
+	fi
 done
 
 echo "INFO - Validating kustomize overlays"
 find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-  do
-    echo "INFO - Validating kustomization ${file/%$kustomize_config}"
-    kustomize build "${file/%$kustomize_config}" "${kustomize_flags[@]}" | \
-      kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}"
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
-    fi
+do
+	echo "INFO - Validating kustomization ${file/%$kustomize_config}"
+	kustomize build "${file/%$kustomize_config}" "${kustomize_flags[@]}" | \
+	  kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}"
+	if [[ ${PIPESTATUS[0]} != 0 ]]; then
+	  exit 1
+	fi
 done
