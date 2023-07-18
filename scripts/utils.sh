96444f7de2d12a435881557bf5369ce0b528ba70
Darío Blanco <dblancoit@gmail.com>
2023-07-18 22:38:09 +0200

    chore: remove newlines from utils

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -3,27 +3,27 @@
 reset_color='\033[0m'
 
 function print_blue() {
-  echo -e "⌛️ \033[34m${1}${reset_color}\n"
+  echo -e "⌛️ \033[34m${1}${reset_color}"
 }
 
 function print_cyan() {
-  echo -e "🎉 \033[36m${1}${reset_color}\n"
+  echo -e "🎉 \033[36m${1}${reset_color}"
 }
 
 function print_green() {
-  echo -e "✅ \033[32m${1}${reset_color}\n"
+  echo -e "✅ \033[32m${1}${reset_color}"
 }
 
 function print_magenta() {
-  echo -e "🤨 \033[35m${1}${reset_color}\n"
+  echo -e "🤨 \033[35m${1}${reset_color}"
 }
 
 function print_red() {
-  echo -e "🚨 \033[31m${1}${reset_color}\n"
+  echo -e "🚨 \033[31m${1}${reset_color}"
 }
 
 function print_yellow() {
-  echo -e "📣 \033[33m${1}${reset_color}\n"
+  echo -e "📣 \033[33m${1}${reset_color}"
 }
 
 function exit_gracefully() {
